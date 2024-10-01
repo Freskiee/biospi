bacba98bef1e902ae34925e6d7a229bbc108ab57
Ariel Sanchez <freska_96@hotmail.com>
2024-10-01 03:51:21 -0600

    feat: arreglando los estilos de las imagenes de mision, vision y valores, igual responsiva

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,6 +1,9 @@
 import '../Introduction/styles.css';
 import biospiImage from '../../img/biospi_img.png'; // Imagen del encabezado de la introducción
 import perrito from '../../img/perrito.png'; // Imagen de la compañía
+import misionImage from '../../img/mision_img.png';
+import visionImage from '../../img/vision_img.png';
+import valoresImage from '../../img/valores_img.png';
 
 export const Introduction = () => {
     return (
@@ -16,7 +19,7 @@ export const Introduction = () => {
                         <div className="intro-image">
                             <img src={perrito} alt="Imagen Perrito" className='company-image' />
                         </div>
-                        
+
                         {/* Texto de introducción */}
                         <div className="intro-text">
                             <h3><b>¿Quiénes somos?</b></h3>
@@ -24,7 +27,7 @@ export const Introduction = () => {
                             <h3><b>En BIOSPI, somos amantes de todos</b><br /> <b>los animales...</b></h3>
                             <br />
                             <p>
-                                Los respetamos y les damos un valor mayor al de una simple mascota, 
+                                Los respetamos y les damos un valor mayor al de una simple mascota,
                                 que forman parte de nuestras vidas, convirtiéndose así en un miembro importante de la familia.
                             </p>
                             <p>
@@ -63,35 +66,35 @@ export const Introduction = () => {
             {/* Sección de misión, visión y valores */}
             <section className="company-info">
                 {/* Columna de misión */}
-                <div className="info-column mision">
-                    <h3>MISIÓN</h3>
-                    <p>
-                        <span>Biospi tiene un claro enfoque al trato digno, respetuoso y amoroso; que debe recibir tu pequeño compañero, 
-                        ofreciendo productos y servicios de una excelente calidad.</span>
-                    </p>
+                <div className="info-column">
+                    <div className="info-header">
+                        <img src={misionImage} alt="Misión" className="info-image" /> {/* Imagen para Misión */}
+                        <h3>MISIÓN</h3>
+                    </div>
+                    <p><span>Biospi tiene un claro enfoque al trato digno, respetuoso y amoroso; que debe recibir tu pequeño compañero, ofreciendo productos y servicios de una excelente calidad.</span></p>
                 </div>
-
                 {/* Columna de visión */}
-                <div className="info-column vision">
-                    <h3>VISIÓN</h3>
-                    <p>
-                        <span>¡Diversificación en nuestros servicios! Biospi también es un espacio en el cual encontrarás: hospedaje para tus 
-                        compañeritos de vida en nuestro Resort Canino, servicio de estética, spa, venta de accesorios, alimentos, área de entrenamiento 
-                        canino y muchas cosas más.</span>
-                    </p>
-                    <p>“En Biospi trabajamos en brindar un servicio de excelencia y mejorar cada día”</p>
+                <div className="info-column">
+                    <div className="info-header">
+                        <img src={visionImage} alt="Visión" className="info-image" /> {/* Imagen para Visión */}
+                        <h3>VISIÓN</h3>
+                    </div>
+                    <p><span>¡Diversificación en nuestros servicios! Biospi también es un espacio en el cual encontrarás: hospedaje para tus compañeritos de vida en nuestro Resort Canino, servicio de estética, spa, venta de accesorios, alimentos, área de entrenamiento canino y muchas cosas más.</span></p>
+                    <p className='eslogan'>“En Biospi trabajamos en brindar un servicio de excelencia y mejorar cada día”</p>
                 </div>
-
                 {/* Columna de valores */}
-                <div className="info-column valores">
-                    <h3>VALORES</h3>
-                    <p>
-                        <span>Compromiso</span><br />
-                        <span>Eficiencia</span><br />
-                        <span>Efectividad</span><br />
-                        <span>Empatía</span><br />
-                        <span>Simplicidad</span><br />
-                        <span>Calidez</span><br />
+                <div className="info-column">
+                    <div className="info-header">
+                        <img src={valoresImage} alt="Valores" className="info-image" /> {/* Imagen para Valores */}
+                        <h3>VALORES</h3>
+                    </div>
+                    <p className="valores-text">
+                        <span>Compromiso</span>
+                        <span>Eficiencia</span>
+                        <span>Efectividad</span>
+                        <span>Empatía</span>
+                        <span>Simplicidad</span>
+                        <span>Calidez</span>
                         <span>Conexión con la naturaleza</span>
                     </p>
                 </div>
